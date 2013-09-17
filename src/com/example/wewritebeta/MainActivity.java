ebfb58f12271c1c8a8e07e9a239e0a8c0879a426
MattBaum <mattbaumgartner@192.168.1.120>
2013-09-16 20:53:02 -0400

    Added listener and methods for undo/redo. Currently there is a bug with them that causes a fatal error.

--- src/com/example/wewritebeta/MainActivity.java
+++ src/com/example/wewritebeta/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.wewritebeta;
 
-import android.os.Bundle;
+import java.util.Calendar;
+import java.util.Stack;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
+
+  
+  Stack<CharSequence> undoStack = new Stack<CharSequence>();
+  Stack<CharSequence> redoStack = new Stack<CharSequence>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -23,9 +34,67 @@ public class MainActivity extends Activity
     return true;
   }
   
-  public void weWriteTest()
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+   
+  CharSequence replaceText;
+  EditText currentText = (EditText) findViewById(R.id.edit_message);
+      switch (item.getItemId()) 
+      {
+      case R.id.action_undo:
+          replaceText = undoStack.pop();
+          redoStack.push(currentText.getText());
+          currentText.setText(replaceText);
+      case R.id.action_redo:
+          replaceText = redoStack.pop();
+          undoStack.push(currentText.getText());
+          currentText.setText(replaceText);
+          }
+           
+      return super.onOptionsItemSelected(item);
+      } 
+  
+  public void Listener()
   {
     
+    
+    EditText messageText = (EditText) findViewById(R.id.edit_message);
+    messageText.addTextChangedListener(new TextWatcher() 
+    {
+      private int startingTime = 0;
+      String changeHolder;
+      
+      public void afterTextChanged(Editable s) 
+      {
+      }
+      
+      public void beforeTextChanged(CharSequence s, int start, 
+        int count, int after) 
+      {
+      }
+      
+      public void onTextChanged(CharSequence s, int start, 
+          int before, int count)
+      {
+        Calendar cal = Calendar.getInstance();
+        int second = cal.get(Calendar.SECOND);
+        
+        if(startingTime == 0)
+        {
+          startingTime = second;
+          
+        }
+        if( second - startingTime > 2 )
+        {
+          undoStack.push(s);
+          startingTime = second; 
+        }
+         
+        
+        
+      }
+
+    });
   }
 
 }
