514e00b322071df55d9f74c994fe23d7163d9786
OwenWeng <owenweng@umich.edu>
2013-09-25 23:24:22 -0400

    Finish Collabrify for sessions with a fetal error

--- src/com/example/wewritebeta/MainActivity.java
+++ src/com/example/wewritebeta/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.wewritebeta;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Stack;
 
@@ -7,30 +10,78 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.util.Log;
+import edu.umich.imlc.collabrify.client.CollabrifyClient;
+import edu.umich.imlc.collabrify.client.exceptions.CollabrifyException;
 
-public class MainActivity extends Activity
-{
 
+public class MainActivity extends Activity {
+
+  // variables for layout
+  public Button createSessionButton;
+  public Button joinSeesionButton;
+  public Button leaveSessionButton;
+  public CheckBox baseFile;
+  
+  //variables for Collabrify 
+  public final String TAG = "WeWrite";
+  public CollabrifyClient myClient;
+  public long sessionId;
+  public String sessionName;
+  public ArrayList<String> tags = new ArrayList<String>();
+  public ByteArrayInputStream baseFileBuffer;
+  public ByteArrayOutputStream baseFileReceiveBuffer;
+  public String broadcastedText;
+  
+  // variables for undo/redo  
   public static boolean ButtonChanged = false;
   Stack<CharSequence> undoStack = new Stack<CharSequence>();
   Stack<CharSequence> redoStack = new Stack<CharSequence>();
+  
 
   @Override
-  protected void onCreate(Bundle savedInstanceState)
-  {
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    
+    createSessionButton = (Button) findViewById(R.id.createSessionButton);
+    joinSeesionButton = (Button) findViewById(R.id.joinSessionButton);
+    leaveSessionButton = (Button) findViewById(R.id.leaveSessionButton);
+    baseFile = (CheckBox) findViewById(R.id.baseFile);
+    
+    tags.add("test");
+    
+    // Instantiate client object
+    boolean getLatestEvent = false;
+    try
+    {
+      myClient = new CollabrifyClient(this, "user email", "user display name",
+          "441fall2013@umich.edu", "XY3721425NoScOpE", getLatestEvent,
+          new WeWriteCollabrifyAdapter(this));
+    }
+    catch(CollabrifyException e)
+    {
+      e.printStackTrace();
+    }
+    
+    createSessionButton.setOnClickListener(new CreateListener(this));
+    joinSeesionButton.setOnClickListener(new JoinListener(this)); 
+    leaveSessionButton.setOnClickListener(new LeaveListener(this));
+    
+    
+    
+    
     TextListener();
   }
 
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
+  public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
     return true;
@@ -39,8 +90,8 @@ public class MainActivity extends Activity
   @Override
   public boolean onOptionsItemSelected(MenuItem item) 
   {
-  CharSequence replaceText;
-  EditText currentText = (EditText) findViewById(R.id.edit_message);
+      CharSequence replaceText;
+      EditText currentText = (EditText) findViewById(R.id.edit_message);
       switch (item.getItemId()) 
       {
         
